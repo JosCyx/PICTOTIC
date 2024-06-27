ee8d79d57c101d4b3cc84f4aaacc1f90b1badcc9
JosCyx <joseguillermojm.jm@gmail.com>
2024-06-26 21:54:31 -0500

    datos agregados

---
+++ src/assets/data/roles.ts
@@ -0,0 +1,7 @@
+
+export const ROLES = [
+    { id: 1, name: 'Admin' },
+    { id: 2, name: 'Teacher' },
+    { id: 3, name: 'Guest' },
+  ];
+  
---
+++ src/assets/data/users.ts
@@ -0,0 +1,6 @@
+export const USERS = [
+    { id: 1, name: 'Admin', username: 'admin', password : '321321', role: 1},
+    { id: 2, name: 'Teacher', username: 'teacher', password : '321321', role: 2},
+    { id: 3, name: 'Guest', username: 'guest', password : '321321', role: 3},
+]
+
